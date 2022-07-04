a22798f5f6c7bf1731a7c78f402a3ca236d01be3
Sebastian Ruiz <74436915+sebastianruizm@users.noreply.github.com>
2022-07-03 22:25:22 -0300

    Update setup-env.sh

--- scripts/bash/setup-env.sh
+++ scripts/bash/setup-env.sh
@@ -4,6 +4,13 @@
 nohup $KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties & > /dev/null && sleep 10
 nohup $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties & > /dev/null && sleep 10
 
+$KAFKA_HOME/bin/kafka-topics.sh \
+--create \
+--zookeeper localhost:2181 \
+--replication-factor 1 \
+--partitions 1 \
+--topic spark-demo-events
+
 # CASSANDRA
 nohup $CASSANDRA_HOME/bin/cassandra & > /dev/null && sleep 10
 
